ff49d3cefa917e0d56e48e08d505ed66eb5b97a7
Stephane Mansour <stephane.mansour@gmail.com>
2017-05-23 15:23:47 +0100

    seed added to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+25.times do
+  Article.create(title: Faker::ChuckNorris.fact, content: Faker::Lorem.paragraph(2))
+end
